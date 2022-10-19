1d44d449e3e7ead6270f721f47ca588133741786
FelipeAbello <felipe.abello2201@alumnos.ubiobio.cl>
2022-10-18 22:05:34 -0300

    agregado controlador

--- src/Controlador/ControladorArriendoEquipos.java
+++ src/Controlador/ControladorArriendoEquipos.java
@@ -1,4 +1,30 @@
 package Controlador;
 
 public class ControladorArriendoEquipos {
+    private static ControladorArriendoEquipos instance = null;
+    private ControladorArriendoEquipos(){
+
+    }
+    public static ControladorArriendoEquipos getInstance(){
+
+        if(instance == null){
+            instance = new ControladorArriendoEquipos();
+        }
+        return instance;
+    }
+    public void creaCliente(String rut, String nom, String dir, String tel){
+
+    }
+    public void creaEquipo(String cod, String desc, long precio){
+
+    }
+    public String[][] listaClientes(){
+
+        return   ;
+    }
+    public String[][] listaEquipos(){
+
+        return   ;
+    }
+
 }
