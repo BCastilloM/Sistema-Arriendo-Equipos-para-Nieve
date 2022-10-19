9b9631d77ca97dd1ce036d9bf69c74bd808f567a
Benjamín <benjamin.castillo2201@alumnos.ubiobio.cl>
2022-10-18 23:29:13 -0300

    Avance clase UIArriendoEquipos

--- src/Controlador/ControladorArriendoEquipos.java
+++ src/Controlador/ControladorArriendoEquipos.java
@@ -1,13 +1,9 @@
 package Controlador;
 
-import java.sql.SQLOutput;
-import java.util.Scanner;
 public class ControladorArriendoEquipos {
     private static ControladorArriendoEquipos instance = null;
-    private final Scanner scan;
     private ControladorArriendoEquipos(){
-        scan = new Scanner(System.in);
-        scan.useDelimiter("[\t|\r\n]+");
+
     }
     public static ControladorArriendoEquipos getInstance(){
 
@@ -17,38 +13,18 @@ public class ControladorArriendoEquipos {
         return instance;
     }
     public void creaCliente(String rut, String nom, String dir, String tel){
-        System.out.println("Ingrese Rut del cliente");
-        rut = scan.next();
-        System.out.println("Ingrese Nombre del cliente");
-        nom = scan.next();
-        System.out.println("Ingrese Direccion del cliente");
-        dir = scan.next();
-        System.out.println("Ingrese Telefono del cliente");
-        tel = scan.next();
+
     }
     public void creaEquipo(String cod, String desc, long precio){
-        System.out.println("Ingrese Codigo del equipo");
-        cod = scan.next();
-        System.out.println("Ingrese Descripcion del equipo");
-        desc = scan.next();
-        System.out.println("Ingrese Precio de arriendo por dia del equipo");
-        precio = scan.nextLong();
-    }
-    /*
 
+    }
     public String[][] listaClientes(){
-        //Figura 5 (?
 
         return   ;
     }
-    */
-
-    /*
     public String[][] listaEquipos(){
-        //Figura 6 (?
 
         return   ;
     }
-     */
 
 }
--- src/Vista/UIArriendoEquipos.java
+++ src/Vista/UIArriendoEquipos.java
@@ -1,4 +1,60 @@
 package Vista;
 
+import java.util.Scanner;
 public class UIArriendoEquipos {
+    private UIArriendoEquipos instance=null;
+    private final Scanner scan;
+
+    private UIArriendoEquipos() {
+        scan = new Scanner(System.in);
+        scan.useDelimiter("[\t|\r\n]+");
+    }
+
+    public UIArriendoEquipos getInstance() {
+        if(instance == null) {
+            instance = new UIArriendoEquipos();
+        }
+        return instance;
+    }
+
+    public void menu() {
+        int opcion=0;
+
+        do{
+            System.out.println("******* SISTEMA DE ARRIENDO DE EQUIPOS DE NIEVE ******");
+            System.out.println("*** MENÚ DE OPCIONES ***");
+            System.out.println("1.- Crea un nuevo cliente");
+            System.out.println("2.- Crea un nuevo equipo");
+            System.out.println("3.- Lista todos los clientes");
+            System.out.println("4.- Lista todos los equipos");
+            System.out.println("5.- Salir");
+            System.out.print("\tIngrese opción: ");
+                opcion = scan.nextInt();
+
+            switch (opcion) {
+                case 1 -> creaCliente();
+                case 2 -> creaEquipo();
+                case 3 -> listaClientes();
+                case 4 -> listaEquipos();
+                case 5 -> {}
+                default -> System.out.println("Error! El valor no está dentro del rango válido");
+            }
+        }while(opcion != 5);
+    }
+
+    public void creaCliente() {
+
+    }
+
+    public void creaEquipo() {
+
+    }
+
+    public void listaClientes() {
+
+    }
+
+    public void listaEquipos() {
+
+    }
 }
