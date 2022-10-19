f64d7dd0c94df6d45e336ecf0ad50e4706377507
Benjamín <benjamin.castillo2201@alumnos.ubiobio.cl>
2022-10-18 21:36:29 -0300

    Avance clase Cliente. enum EstadoEquipo terminada.

--- src/Modelo/Cliente.java
+++ src/Modelo/Cliente.java
@@ -1,4 +1,50 @@
 package Modelo;
 
 public class Cliente {
+    // Atributos
+    private String rut;
+    private String nombre;
+    private String direccion;
+    private String telefono;
+    private boolean activo;
+
+    // Constructor
+    public Cliente(String rut, String nombre, String direccion, String telefono) {
+        this.rut = rut;
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.telefono = telefono;
+    }
+
+    // Métodos
+    public String getRut() {
+        return rut;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    /*
+        ***POR HACER***
+    public boolean isActivo() {
+        return activo;
+    }
+
+    public void setActivo(boolean activo) {
+        this.activo = activo;
+    }
+
+    public void setInactivo() {
+
+    }
+    */
 }
