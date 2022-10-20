4c7e15713425ddf660b535150b754e595f2b90ab
angel <angel.montecino2201@alumnos.ubiobio.cl>
2022-10-19 23:10:49 -0300

    Agregando comentario de los integrantes

--- src/Vista/Main.java
+++ src/Vista/Main.java
@@ -1,5 +1,8 @@
 package Vista;
-
+// Integrantes:
+// Benjamín Castillo
+//Ángel Montecino
+//Felipe Abello
 public class Main {
     public static void main(String[] args) {
         UIArriendoEquipos.getInstance().menu();
