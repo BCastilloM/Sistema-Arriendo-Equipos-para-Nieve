d6bc75d0aa095be2d70df0f29ddf31fd49cae6d8
Benjamín <benjamin.castillo2201@alumnos.ubiobio.cl>
2022-10-19 21:46:19 -0300

    Arreglando clase Equipo.

--- src/Modelo/Equipo.java
+++ src/Modelo/Equipo.java
@@ -5,7 +5,7 @@ public class Equipo {
     private long codigo;
     private String descripcion;
     private long precioArriendoDia;
-    private EstadoEquipo estado;
+    private EstadoEquipo estado = EstadoEquipo.OPERATIVO;
 
     public Equipo(long codigo, String descripcion, long precioArriendoDia) {
         this.codigo = codigo;
