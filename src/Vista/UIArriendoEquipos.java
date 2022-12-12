72986ad16a145a25cab9548cdda92f7ead1abdc2
FelipeAbello <felipe.abello2201@alumnos.ubiobio.cl>
2022-12-12 18:13:50 -0300

    Otro pequeño ajuste en "arriendaEquipos"

--- src/Vista/UIArriendoEquipos.java
+++ src/Vista/UIArriendoEquipos.java
@@ -15,10 +15,9 @@ import Modelo.EstadoEquipo;
 import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.Scanner;
-
 public class UIArriendoEquipos {
     // Atributos
-    private static UIArriendoEquipos instance = null;
+    private static UIArriendoEquipos instance=null;
     private final Scanner scan;
 
     //Constructor
@@ -31,7 +30,7 @@ public class UIArriendoEquipos {
 
     // Crea objeto de tipo UIArriendoEquipos
     public static UIArriendoEquipos getInstance() {
-        if (instance == null) {
+        if(instance == null) {
             instance = new UIArriendoEquipos();
         }
         return instance;
@@ -40,8 +39,7 @@ public class UIArriendoEquipos {
     public void menu() {
         int opcion;
 
-
-        do {
+        do{
             System.out.println("\n\n\n******* SISTEMA DE ARRIENDO DE EQUIPOS DE NIEVE *******");
             System.out.println("*** MENÚ DE OPCIONES ***");
             System.out.println("1. Crea un nuevo cliente");
@@ -73,11 +71,10 @@ public class UIArriendoEquipos {
                 case 7 -> listaEquipos();
                 case 8 -> listaArriendos();
                 case 9 -> listaDetallesArriendo();
-                case 10 -> {
-                }
+                case 10 -> {}
                 default -> System.out.println("Error! El valor no está dentro del rango válido");
             }
-        } while (opcion != 10);
+        }while(opcion != 10);
     }
 
     private void creaCliente() {
@@ -128,37 +125,38 @@ public class UIArriendoEquipos {
         System.out.print("\nRut cliente: ");
         rutCliente = scan.next();
         String[] cliente = ControladorArriendoEquipos.getInstance().consultaCliente(rutCliente);
-        try {
+        try{
             codigoArriendo = ControladorArriendoEquipos.getInstance().creaArriendo(rutCliente);
-        } catch (ClienteException e) {
+        }catch(ClienteException e){
             System.out.println(e.getMessage());
             return;
         }
         System.out.print("\nNombre cliente: " + cliente[1]);
+
         do {
             System.out.print("\nCódigo equipo: ");
             codigoEquipo = scan.nextLong();
             String[] datosEquipo = ControladorArriendoEquipos.getInstance().consultaEquipo(codigoEquipo);
-            if (datosEquipo[3].equals("entregado")) {
+            if(datosEquipo[3].equals("entregado")){
                 System.out.print("\nEl equipo se encuentra arrendado");
-            } else {
-                try {
+            }else{
+                try{
                     ControladorArriendoEquipos.getInstance().agregaEquipoToArriendo(codigoArriendo, codigoEquipo);
-                } catch (ArriendoException | EquipoException e) {
+                }catch(ArriendoException | EquipoException e){
                     System.out.println(e.getMessage());
                     return;
                 }
                 System.out.print("\nSe ha agregado " + datosEquipo[1] + " al arriendo");
-                try {
-                    precioTotal += ControladorArriendoEquipos.getInstance().cierraArriendo(codigoArriendo);
-                } catch (ArriendoException e) {
-                    System.out.println(e.getMessage());
-                    return;
-                }
             }
             System.out.print("\n¿Desea agregar otro equipo al arriendo? (s/n)");
             pregunta = scan.next();
-        } while (pregunta.equals("s"));
+        }while(pregunta.equals("s"));
+        try{
+            precioTotal=ControladorArriendoEquipos.getInstance().cierraArriendo(codigoArriendo);
+        }catch(ArriendoException e){
+            System.out.println(e.getMessage());
+            return;
+        }
         System.out.print("\nMonto total por día de arriendo: --> $" + precioTotal);
     }
 
@@ -174,7 +172,7 @@ public class UIArriendoEquipos {
             arriendos = ControladorArriendoEquipos.getInstance().listaArriendosPorDevolver(rutCliente);
         } catch (ClienteException e) {
             System.out.println(e.getMessage());
-            return;
+            return ;
         }
         System.out.print("\nNombre cliente: " + cliente[1]);
         System.out.println("");
@@ -191,21 +189,21 @@ public class UIArriendoEquipos {
         System.out.println("");
         System.out.print("Ingrese código y estado en que se devuelve cada equipo que se indica >>>");
         int opcion;
-        for (int i = 0; i < detalleArriendo.length; i++) {
-            System.out.print("\n(" + detalleArriendo[i][1] + ") -> Estado (1: Operativo, 2: En Reparacion, 3: Dado de baja): ");
-            opcion = scan.nextInt();
-            switch (opcion) {
-                case 1 -> estadoEquipo[i] = EstadoEquipo.OPERATIVO;
-                case 2 -> estadoEquipo[i] = EstadoEquipo.EN_REPARACION;
-                case 3 -> estadoEquipo[i] = EstadoEquipo.DADO_DE_BAJA;
+        for(int i = 0; i < detalleArriendo.length; i++){
+        System.out.print("\n(" + detalleArriendo[i][1] + ") -> Estado (1: Operativo, 2: En Reparacion, 3: Dado de baja): ");
+        opcion = scan.nextInt();
+        switch(opcion){
+            case 1 -> estadoEquipo[i] = EstadoEquipo.OPERATIVO;
+            case 2 -> estadoEquipo[i] = EstadoEquipo.EN_REPARACION;
+            case 3 -> estadoEquipo[i] = EstadoEquipo.DADO_DE_BAJA;
             }
         }
-        try {
+        try{
             ControladorArriendoEquipos.getInstance().devuelveEquipos(arriendoADevolver, estadoEquipo);
             System.out.print("\n\n" + detalleArriendo.length + "equipo(s) fue(ron) devuelto(s) exitosamente");
-        } catch (ArriendoException e) {
+        }catch (ArriendoException e){
             System.out.println(e.getMessage());
-            return;
+            return ;
         }
     }
 
@@ -214,9 +212,9 @@ public class UIArriendoEquipos {
         System.out.println("Cambiando el estado a un cliente...");
         System.out.print("Rut cliente: ");
         rutCliente = scan.next();
-        try {
+        try{
             ControladorArriendoEquipos.getInstance().cambiaEstadoCliente(rutCliente);
-        } catch (ClienteException e) {
+        }catch(ClienteException e){
             System.out.println(e.getMessage());
             return;
         }
@@ -226,29 +224,29 @@ public class UIArriendoEquipos {
 
     private void listaClientes() {
         String[][] listadoClientes = ControladorArriendoEquipos.getInstance().listaClientes();
-        if (listadoClientes.length > 0) {
+        if(listadoClientes.length > 0){
             System.out.println("LISTADO DE CLIENTES");
             System.out.println("-------------------\n");
-            System.out.printf("%-15s%-20s%-20s%-16s%-8s%-14s%n", "Rut", "Nombre", "Direccion", "Telefono", "Estado", "Nro.Arr.Pdtes.");
-            for (String[] columna : listadoClientes) {
+            System.out.printf("%-15s%-20s%-20s%-16s%-8s%-14s%n", "Rut", "Nombre", "Direccion",  "Telefono", "Estado", "Nro.Arr.Pdtes.");
+            for(String[] columna : listadoClientes){
                 System.out.printf("%-15s%-20s%-20s%-16s%-8s%14s%n", columna[0], columna[1], columna[2], columna[3], columna[4], columna[5]);
             }
 
-        } else {
+        }else{
             System.out.println("No se han registrado clientes");
         }
     }
 
     private void listaEquipos() {
         String[][] datosEquipos = ControladorArriendoEquipos.getInstance().listaEquipos();
-        if (datosEquipos.length > 0) {
+        if(datosEquipos.length > 0) {
             System.out.println("LISTADO DE EQUIPOS");
             System.out.println("------------------");
             System.out.printf("%-15s%-50s%-20s%-15s%-15s%n", "Código", "Descripción", "Precio", "Estado", "Situacion");
-            for (String[] columna : datosEquipos) {
+            for(String[] columna : datosEquipos) {
                 System.out.printf("%-15s%-50s%,-20d%-15s%-15s%n", columna[0], columna[1], Long.parseLong(columna[2]), columna[3], columna[4]);
             }
-        } else {
+        }else {
             System.out.println("No se han registrado equipos");
         }
     }
@@ -266,8 +264,8 @@ public class UIArriendoEquipos {
         System.out.println("--------------------\n");
         System.out.printf("%-8s%-15s%-15s%-12s%-15s%-12s%n", "Codigo", "Fecha inicio", "Fecha devol.", "Estado", "Rut cliente", "Monto total");
         int i = 0;
-        for (String[] columna : datosArriendos) {
-            if (LocalDate.parse(datosArriendos[i][1].replace("/", "-")).isAfter(localFechaInicio) && LocalDate.parse(datosArriendos[i][2].replace("/", "-")).isBefore(localFechaFin)) {
+        for(String[] columna : datosArriendos){
+            if(LocalDate.parse(datosArriendos[i][1].replace("/","-")).isAfter(localFechaInicio) && LocalDate.parse(datosArriendos[i][2].replace("/","-")).isBefore(localFechaFin)){
                 System.out.printf("%-8s%-15s%-15s%-12s%-15s%12s%n", datosArriendos[i][0], datosArriendos[i][1], datosArriendos[i][2], datosArriendos[i][3], datosArriendos[i][4], datosArriendos[i][6]);
             }
             i++;
@@ -292,7 +290,7 @@ public class UIArriendoEquipos {
         System.out.print("\n\t\t\t\t\t\tDETALLE DEL ARRIENDO");
         System.out.print("\n----------------------------------------------------------------");
         System.out.printf("%-15s%-25s%-25s%n", "Codigo equipo", "Descripcion equipo", "Precio arriendo por dia");
-        for (int i = 0; i < detalleArriendo.length; i++) {
+        for(int i = 0; i<detalleArriendo.length; i++){
             System.out.printf("%-15s%-25s%25s%n", detalleArriendo[i][0], detalleArriendo[i][1], detalleArriendo[i][2]);
         }
     }
