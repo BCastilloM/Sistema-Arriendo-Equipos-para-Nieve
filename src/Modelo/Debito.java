9e27691ffde9394886e4f3e495c2b88b55308963
Benjamín <benjamin.castillo2201@alumnos.ubiobio.cl>
2022-12-17 19:24:36 -0300

    Arreglando clase Debito y Contado.

--- src/Modelo/Debito.java
+++ src/Modelo/Debito.java
@@ -1,12 +1,12 @@
 package Modelo;
 
-import java.util.Date;
+import java.time.LocalDate;
 
 public class Debito extends Pago {
     private String codTransaccion;
     private String numTarjeta;
 
-    public Debito(long monto, Date fecha, String codTransaccion, String numTarjeta) {
+    public Debito(long monto, LocalDate fecha, String codTransaccion, String numTarjeta) {
         super(monto, fecha);
         this.codTransaccion = codTransaccion;
         this.numTarjeta = numTarjeta;
